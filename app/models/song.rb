66f6a517d3c24e0b5c36ff706ac531eda8a36801
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-01-18 11:50:21 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
-      Genre.all.find do |genre|
-          genre.id = self.genre_id
-      end.name
+      self.genre.name
   end
 
   def drake_made_this
